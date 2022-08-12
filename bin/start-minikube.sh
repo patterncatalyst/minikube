1e6aff147c78dae509bd72271fa60469b8fbe3da
Robert Sedor <rsedor2007@gmail.com>
2022-08-12 17:27:20 -0400

    added prereq and install pages

---
+++ bin/start-minikube.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eu
+
+# change the name of the profile for example knativesandbox
+PROFILE_NAME=${PROFILE_NAME:-mysandbox}
+MEMORY=${MEMORY:-16384}
+CPUS=${CPUS:-6}
+unamestr=$(uname)
+
+if [ "$unamestr" == "Darwin" ];
+then
+  minikube start -p "$PROFILE_NAME" \
+  --memory="$MEMORY" \
+  --driver=hyperkit \
+  --cpus="$CPUS" \
+  --kubernetes-version=v1.23.0 \
+  --disk-size=50g \
+  --insecure-registry='10.0.0.0/24'
+else
+  minikube start -p "$PROFILE_NAME" \
+  --memory="$MEMORY" \
+  --cpus="$CPUS" \
+  --kubernetes-version=v1.23.0 \
+  --disk-size=50g \
+  --insecure-registry='10.0.0.0/24'
+fi
