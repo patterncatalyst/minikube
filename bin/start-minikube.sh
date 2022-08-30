342975cd90d882a596c1d67e1ee758ff1869f80a
Robert Sedor <rsedor2007@gmail.com>
2022-08-30 12:53:52 -0400

    updated startup script

--- bin/start-minikube.sh
+++ bin/start-minikube.sh
@@ -3,13 +3,12 @@
 set -eu
 
 # change the name of the profile for example knativesandbox
-PROFILE_NAME=${PROFILE_NAME:-mysandbox}
-MEMORY=${MEMORY:-16384}
-CPUS=${CPUS:-6}
+PROFILE_NAME=mysandbox
+MEMORY=16384
+CPUS=6
 unamestr=$(uname)
 
-if [ "$unamestr" == "Darwin" ];
-then
+if [ "$unamestr" == "Darwin" ]; then
   minikube start -p "$PROFILE_NAME" \
   --memory="$MEMORY" \
   --driver=hyperkit \
@@ -17,11 +16,18 @@ then
   --kubernetes-version=v1.24.3 \
   --disk-size=50g \
   --insecure-registry='10.0.0.0/24'
-else
-  minikube start -p "$PROFILE_NAME" \
-  --memory="$MEMORY" \
-  --cpus="$CPUS" \
+elif [ "$unamestr" == "Linux" ]; then
+  /usr/bin/minikube start -p $PROFILE_NAME \
+  --driver=docker \
+  --memory $MEMORY \
+  --cpus $CPUS \
   --kubernetes-version=v1.24.3 \
   --disk-size=50g \
   --insecure-registry='10.0.0.0/24'
+
+  /usr/bin/minikube -p $PROFILE_NAME addons enable metrics-server
+  /usr/bin/minikube -p $PROFILE_NAME addons enable dashboard
+  /usr/bin/minikube -p $PROFILE_NAME addons enable ingress
+  /usr/bin/minikube -p $PROFILE_NAME addons enable storage-provisioner
 fi
+
